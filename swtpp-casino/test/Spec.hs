07b11e17d5c1c292c72ef0212d67300d9cdc97b8
jherzog99 <herzog.2@campus.tu-berlin.de>
2022-11-15 12:35:21 +0100

    added solution for 3.b)

--- swtpp-casino/test/Spec.hs
+++ swtpp-casino/test/Spec.hs
@@ -1,2 +1,12 @@
 import Test.Hspec
 import Command
+
+-- Aufgabe 3.b)
+main :: IO ()
+main = hspec $ do
+    describe "Command.toCommand" $ do
+        it "creates a Command from Integer" $ do
+            eval (toCommand 5) `shouldBe` (5 :: Int)
+    describe "Command.eval" $ do
+        it "evaluate Put" $
+            eval (Put (toCommand 3) (toCommand 5)) `shouldBe`  (7 :: Int)

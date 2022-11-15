7c7dee6d07f97663c2ccb7de27574f9b667d4fae
jherzog99 <herzog.2@campus.tu-berlin.de>
2022-11-15 14:05:21 +0100

    added solution for 2.b)

--- swtpp-casino/test/Spec.hs
+++ swtpp-casino/test/Spec.hs
@@ -10,3 +10,19 @@ main = hspec $ do
     describe "Command.eval" $ do
         it "evaluate Put" $
             eval (Put (toCommand 3) (toCommand 5)) `shouldBe`  (7 :: Int)
+
+-- Aufgabe 3.c)
+mainC :: IO ()
+mainC = hspec $ do
+    testToCommandC
+    testEvalC
+
+testToCommandC :: Spec
+testToCommandC = describe "Command.toCommand" $ do
+        it "creates a Command from Integer" $ do
+            eval (toCommand 5) `shouldBe` (5 :: Int)
+
+testEvalC :: Spec
+testEvalC = describe "Command.eval" $ do
+        it "evaluate Put" $
+            eval (Put (toCommand 3) (toCommand 5)) `shouldBe`  (7 :: Int)
